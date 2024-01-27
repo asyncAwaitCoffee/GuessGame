6cef794bb2e05b53bf60c70301ca50bf94d7a9f9
Andrei Stepanov <steam.oni.am@gmail.com>
2024-01-27 09:01:23 +0400

    first commit

---
+++ GuessGame/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace GuessGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int secret = 55;
+            int answer = 0;
+            int tries = 10;
+
+            while (answer != secret && tries > 0)
+            { 
+                Console.Write("Guess the number: ");
+                try
+                {
+                    answer = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Incorrect number input!");
+                    continue;
+                }
+
+                tries--;
+
+                if (answer < secret)
+                {
+                    Console.WriteLine("No, it is higher! Try again! " + tries + " " + (tries > 1 ? "tries" : "try") + " left.");
+                }
+                else if (answer > secret)
+                {
+                    Console.WriteLine("No, it is lower! Try again! " + tries + " " + (tries > 1 ? "tries" : "try") + " left.");
+                }
+            }
+
+            if (tries > 0)
+            {
+                Console.WriteLine("Yes, you are right! The number is " + secret + '!');
+            }
+            else
+            {
+                Console.WriteLine("Oh no, you are out of tries!");
+            }
+        }
+    }
+}
